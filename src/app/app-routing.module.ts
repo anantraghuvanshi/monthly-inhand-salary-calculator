307891df086ad4f70a4d0ab985b5c7c33caf904f
Anant <anantsinghraghuvanshi1@gmail.com>
2023-07-06 19:22:57 +0530

    welcome page ts update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome-page/welcome-page.component';
+import { SalaryCalculatorComponent } from './salary-calculator-component/salary-calculator-component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'salary-calculator', component: SalaryCalculatorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-welcome-page',
+  selector: 'app-welcome',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.css']
+  styleUrls: ['./welcome-page.component.css'],
 })
-export class WelcomePageComponent {
+export class WelcomeComponent implements OnInit {
+  userName: string | null = '';
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.userName = localStorage.getItem('userName');
+    if (!this.userName) {
+      this.userName = 'Guest';
+    }
+  }
+
+  goToSalaryCalculator() {
+    this.router.navigate(['salary-calculator']);
+  }
 }
