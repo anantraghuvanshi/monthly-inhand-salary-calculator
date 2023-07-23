7d18947e51ea88300728e711d1dfc0f38750699b
Anant <anantsinghraghuvanshi1@gmail.com>
2023-07-24 01:26:56 +0530

    material UI added

--- src/app/dialog-component/dialog-component.component.ts
+++ src/app/dialog-component/dialog-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export interface DialogData {
@@ -8,6 +8,8 @@ export interface DialogData {
 @Component({
   selector: 'app-dialog-component',
   templateUrl: './dialog-component.component.html',
+  styleUrls: ['./dialog-component.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class DialogComponentComponent {
   constructor(
