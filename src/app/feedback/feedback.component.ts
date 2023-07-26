f5f1937424e40a702aac9752b8cc4c110115ee72
Anant <anantsinghraghuvanshi1@gmail.com>
2023-07-27 01:57:09 +0530

    emailJS added in module

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.css']
+  styleUrls: ['./feedback.component.css'],
 })
-export class FeedbackComponent {
+export class FeedbackComponent implements OnInit {
+  name = '';
+  email = '';
+  feedback = '';
 
+  onSubmit() {
+    const emailData = {
+      name: this.name,
+      email: this.email,
+      feedback: this.feedback,
+    };
+
+    emailjs
+      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', {
+        data: emailData,
+        public_key: 'YOUR_PUBLIC_KEY',
+      })
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
+
+  ngOnInit() {}
 }
